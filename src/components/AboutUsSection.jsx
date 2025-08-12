c59c4a06fb576a7bfb4d570e9c1463db798ad9cc
nazmus <nazmus.up@gmail.com>
2025-08-12 06:43:34 +0600

    Revert "Update AboutUsSection.jsx"
    This reverts commit 07d38eb7167dc51222c4fd07c48abe95141fb21f.

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const AboutUsSection = () => {
   return (
-    <div className="py-16 bg-green-600 md:w-[65%] md:mx-auto md:rounded-2xl shadow-[0px_7px_29px_rgba(100,100,111,0.4)]">
+    <div className="py-16 bg-green-600 md:w-[80%] md:mx-auto md:rounded-2xl shadow-[0px_7px_29px_rgba(100,100,111,0.4)]">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h2 className="text-4xl font-bold text-white mb-6">Our Mission</h2>
         <p className="mb-8 text-xl text-green-100 max-w-4xl mx-auto leading-relaxed">
